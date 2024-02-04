3a5712bb690ee45d1935fb0d7a21386f83b0429e
luz_maria <luz.maria.agc@gmail.com>
2024-02-04 17:16:22 +0100

    refactor: returns_the_number_is_higher_when_number_generated_is_higher_than_guessed_number

--- src/guess_number.py
+++ src/guess_number.py
@@ -4,10 +4,6 @@ class GuessingNumberGame:
         self.random_number = random_number_generator
 
     def guess_number(self, guessed_number: int) -> str:
-        if guessed_number == 1:
-            return "The number is higher"
-        if guessed_number == 2:
-            return "The number is higher"
-        if guessed_number == 3:
+        if guessed_number < self.random_number:
             return "The number is higher"
         return "Correct you won"
