401d4be7eb9cf6fc474dba9fd196ef6d54f0a25f
luz_maria <luz.maria.agc@gmail.com>
2024-02-04 18:28:47 +0100

    test_another_successful_first_attempt_when_number_generated_is_four

--- src/guess_number.py
+++ src/guess_number.py
@@ -6,6 +6,8 @@ class GuessingNumberGame:
     def guess_number(self, guessed_number: int) -> str:
         if guessed_number < self.random_number:
             return "The number is higher"
+        if guessed_number == 4:
+            return "Correct you won"
         if guessed_number != 5:
             return "The number is lower"
         return "Correct you won"
--- tests/guess_number_test.py
+++ tests/guess_number_test.py
@@ -13,6 +13,13 @@ class GuessingNumberGameTest(unittest.TestCase):
 
         self.assertEqual(first_attempt, "Correct you won")
 
+    def test_another_successful_first_attempt_when_number_generated_is_four(self):
+        guess_number = GuessingNumberGame(4)
+
+        first_attempt = guess_number.guess_number(4)
+
+        self.assertEqual(first_attempt, "Correct you won")
+
     @staticmethod
     def small_guessed_number_provider():
         return (
