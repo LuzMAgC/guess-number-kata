398e168d54910b4b53c079ab212bb8f368dd9a43
luz_maria <luz.maria.agc@gmail.com>
2024-02-04 17:03:07 +0100

    test_returns_the_number_is_higher_when_number_generated_is_five_and_guessed_number_is_one

--- src/guess_number.py
+++ src/guess_number.py
@@ -4,4 +4,6 @@ class GuessingNumberGame:
         self.random_number = random_number_generator
 
     def guess_number(self, guessed_number: int) -> str:
+        if guessed_number == 1:
+            return "The number is higher"
         return "Correct you won"
--- tests/guess_number_test.py
+++ tests/guess_number_test.py
@@ -11,3 +11,10 @@ class GuessingNumberGameTest(unittest.TestCase):
         first_attempt = guess_number.guess_number(5)
 
         self.assertEqual(first_attempt, "Correct you won")
+
+    def test_returns_the_number_is_higher_when_number_generated_is_five_and_guessed_number_is_one(self):
+        guess_number = GuessingNumberGame(5)
+
+        first_attempt = guess_number.guess_number(1)
+
+        self.assertEqual(first_attempt, "The number is higher")
