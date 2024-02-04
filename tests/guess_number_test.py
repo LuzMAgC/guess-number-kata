69aafbd9987e1b9df550db7f0809510fef106410
luz_maria <luz.maria.agc@gmail.com>
2024-02-04 19:02:07 +0100

    refactor: parametrize_tests_when_guessed_number_is_correct

--- tests/guess_number_test.py
+++ tests/guess_number_test.py
@@ -6,24 +6,19 @@ from unittest_data_provider import data_provider
 
 class GuessingNumberGameTest(unittest.TestCase):
 
-    def test_a_successful_first_attempt(self):
-        guess_number = GuessingNumberGame(5)
+    @staticmethod
+    def correct_guessed_number_provider():
+        return (
+            (5, 5),
+            (4, 4),
+            (3, 3),
+        )
 
-        first_attempt = guess_number.guess_number(5)
+    @data_provider(correct_guessed_number_provider)
+    def test_returns_you_won_when_number_generated_is_guessed_number(self, random_number, guessed_number):
+        guess_number = GuessingNumberGame(random_number)
 
-        self.assertEqual(first_attempt, "Correct you won")
-
-    def test_another_successful_first_attempt_when_number_generated_is_four(self):
-        guess_number = GuessingNumberGame(4)
-
-        first_attempt = guess_number.guess_number(4)
-
-        self.assertEqual(first_attempt, "Correct you won")
-
-    def test_another_successful_first_attempt_when_number_generated_is_three(self):
-        guess_number = GuessingNumberGame(3)
-
-        first_attempt = guess_number.guess_number(3)
+        first_attempt = guess_number.guess_number(guessed_number)
 
         self.assertEqual(first_attempt, "Correct you won")
 
